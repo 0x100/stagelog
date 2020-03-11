6b5246039f4d309ff01fa805395c56abdb4ded15
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-03-11 22:25:35 +0300

    Create @Stage annotation

---
+++ src/main/java/com/ilysenko/stagelog/annotation/Stage.java
@@ -0,0 +1,15 @@
+package com.ilysenko.stagelog.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Stage {
+
+    String startMessage() default "";
+
+    String finishMessage() default "";
+}
